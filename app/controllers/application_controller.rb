a792f33f727afb578e802368c5a98ccbd693601f
seanlucius <seanlucius22@icloud.com>
2019-03-13 21:33:33 -0400

    sinatra-ar-crub-lad-done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,57 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
+    redirect '/articles'
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    @article = Article.new
+
+    erb :new
+  end
+
+  get '/articles/:id' do
+
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  post '/articles' do
+
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+
+    article = Article.find(params[:id])
+    article.update(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{article.id}"
+
+  end
+
+  delete '/articles/:id' do
+    article = Article.find(params[:id])
+    article.destroy
+
+    redirect '/articles'
+  end
+
+
 end
